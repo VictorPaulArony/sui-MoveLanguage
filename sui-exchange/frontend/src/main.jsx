6b32f9186dbedd7c20c61f27c40e841c726a35bf
victorpaularony <smallypauls@gmail.com>
2025-09-01 23:16:33 +0300

    feat(root): setup WalletProvider and root component

--- sui-exchange/frontend/src/main.jsx
+++ sui-exchange/frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/index.js
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { WalletProvider } from "@suiet/wallet-kit";
+import "./App.css"
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+// wrap your app with WalletProvider
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <WalletProvider>
+      <App />
+    </WalletProvider>
+  </StrictMode>
+);
